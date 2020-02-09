2d12560981057ae7789f1fc8eeefe8c96343b3ee
Administrator <admin@example.com>
2020-02-09 16:20:24 +0800

    学习使用IDEA提交

---
+++ eureka-client-provider/src/main/java/com/qgw/controller/DcController.java
@@ -0,0 +1,24 @@
+package com.qgw.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class DcController {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+//        discoveryClient屏蔽了实现,不管是eureka还是consul都没问题
+        String services = "Services: " + discoveryClient.getServices();
+        log.info("{}", services);
+        return services;
+    }
+
+}
